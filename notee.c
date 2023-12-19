779abe032b824f39a2ffd79d60fa7ed1f02b585a
tom960541 <122672339+tom960541@users.noreply.github.com>
2023-12-20 03:38:07 +0800

    新增 string.h筆記

--- notee.c
+++ notee.c
@@ -28,5 +28,19 @@ int main(){
 這個樣子的話就會跑出assertion failed:(2==3)還有line 23
 
 這就是assert會做的事，他告訴我們什麼東西出錯了，還有哪裡出錯了。放assert來檢查的話就可以更容易的debug。
+2.string.h 
+重要功能
+strlen() 計算字串長度
+可以返回指定的字串長度，但是不包括末尾的空字符 \0
+size_t strlen(const char *str)
+strcpy() 複製字符串
+把一個字串符複製到另一個中
+char *strcpy(char *dest, const char *src);
+strcat() - 連結字符串
+將一個字符串連結到另一個字符串的末尾
+char *strcat(char *dest, const char *src);
+strcmp() - 比較字符串
+比較兩個字符串
+int strcmp(const char *str1, const char *str2);
 
   
